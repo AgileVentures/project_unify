4aebeabc48e6c965dcee34ac7d082e1cc23de301
Tony Butler <tony@Tonys-MacBook-Pro.local>
2016-02-12 19:47:36 +0000

    Wait for the app to start when running cucumber tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,7 +4,20 @@ require 'httparty'
 
 server = ChildProcess.build('rackup', '--port', '9999')
 server.start
-sleep(5)
+#sleep(5)
+
+
+Timeout.timeout(3) do
+  loop do
+    begin
+      HTTParty.get('http://localhost:9999')
+      break
+    rescue Errno::ECONNREFUSED =>
+        try_again
+      sleep 0.1
+    end
+  end
+end
 
 at_exit do
   server.stop
