25fde4e5cb24e5f7c68fcdeea659060d9d420cf2
Zmago Devetak <zmago_devetak@yahoo.com>
2016-02-15 13:06:40 +0000

    Added seed for AdminUser, fixed jumbotron alignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+AdminUser.create!(
+    :email => "admin@admin.com",
+    :password => "asdf1234"
+)
