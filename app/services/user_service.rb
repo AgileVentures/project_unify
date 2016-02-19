fd4843e7eea58ccd3147beb9484357b4d7bf0deb
tochman <thomas@nocebo.se>
2016-02-19 16:51:28 +0100

    added spec for check of subject class

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -3,5 +3,12 @@ class UserService
 
   attrs_for_index :user_name, :created_at
   attrs_for_show :user_name, :created_at
-  attrs_for_form :user_name, :created_at
+  attrs_for_form :user_name
+
+  filter :user_name
+  batch_action :destroy, confirm: true
+
+  def filter_user_name(resources, value)
+    resources.where('user_name LIKE ?', "%#{value}%")
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  it 'should be of class User' do
+    expect(subject.class).to eq User
+  end
+
   describe 'Database table' do
     it { is_expected.to have_db_column :id }
     it { is_expected.to have_db_column :user_name }
