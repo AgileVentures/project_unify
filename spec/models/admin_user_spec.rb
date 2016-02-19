f768f112df68e29dbd18466848ab2f40d4dd573a
Zmago Devetak <zmago_devetak@yahoo.com>
2016-02-19 21:10:09 +0000

    should to is_expected

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -26,9 +26,9 @@ describe AdminUser, type: :model do
   
   it { is_expected.not_to allow_value("").for(:password) }
 
-  it { should validate_length_of(:password).is_at_least(6) }
+  it { is_expected.to validate_length_of(:password).is_at_least(6) }
 
-  it { should validate_uniqueness_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
   
   it { is_expected.to validate_presence_of(:email) }
   
