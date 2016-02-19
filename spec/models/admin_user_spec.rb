21d269c998582ef545d29c3400cafe6702ec659c
Zmago Devetak <zmago_devetak@yahoo.com>
2016-02-19 11:35:57 +0000

    ad AdminUser spec

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -2,9 +2,47 @@ require 'rails_helper'
 
 describe AdminUser, :type => :model do
     
-  it 'require the email' do
-    admin  = build(:admin_user, email: "")
+  before(:each) do 
+    @admin  = build(:admin_user)
+  end
+  
+  it 'should be valid' do
+     expect(@admin).to be_valid
+  end
+  it 'should have an email address' do
+    @admin.email = ""
+    expect(@admin).to_not be_valid
+  end
+  
+  it 'should not have an invalid email address' do
+    emails = ['asdf@ ds.com', '@example.com', 'test me @yahoo.com', 'asdf@example', 'ddd@.d. .d', 'ddd@.d' ]
+    emails.each do |email|
+      @admin.email = email
+      expect(@admin).to_not be_valid 
+    end
+  end
+  
+  it 'should have a valid email address' do
+    emails = ['asdf@ds.com', 'hello@example.uk', 'test1234@yahoo.si', 'asdf@example.eu' ]
+    emails.each do |email|
+      @admin.email = email
+      expect(@admin).to be_valid 
+    end      
+  end
+ 
+  it 'should have a password' do
+    admin = build(:admin_user, password: "")
     expect(admin).to_not be_valid
   end
-    
+  
+  it 'should not have a password shorter than 6 chars' do
+    admin = build(:admin_user, password: "aaa")
+    expect(admin).to_not be_valid
+  end
+  
+  describe '#login_column' do
+    it 'should return a email column' do
+      expect(AdminUser.login_column).to eq :email    
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,4 +54,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  
+  # Add factory girl methods
+  config.include FactoryGirl::Syntax::Methods
 end
