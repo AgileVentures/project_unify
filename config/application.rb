2c0e317cdf004fcd170829e5db0d444cce92282d
Tony Butler <tony@Tonys-MacBook-Pro.local>
2016-02-21 17:29:37 +0000

    Removed some extra linefeeds

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,6 @@ module ProjectUnify
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
@@ -40,5 +39,4 @@ module ProjectUnify
       end
     end
   end
-
 end
