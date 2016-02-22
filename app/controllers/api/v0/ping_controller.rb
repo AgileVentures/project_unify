c12ed00ec051cb60bb34dff45d18f9bc616126e9
tochman <thomas@nocebo.se>
2016-02-22 19:02:58 +0100

    updates to examples and adds users#show

--- app/controllers/api/v0/ping_controller.rb
+++ app/controllers/api/v0/ping_controller.rb
@@ -1,9 +1,9 @@
 class Api::V0::PingController < ApiController
 
-  api :GET, "/ping/", "Respond with 'Pong'"
-  description "Responds with a message containing the word 'Pong'"
-  formats %w(json text)
-  example " 'message': 'Pnng' "
+  api :GET, '/ping', 'Respond with \'Pong\''
+  description 'Responds with a message containing the word \'Pong\''
+  formats %w(json)
+  example %q({ 'message': 'Pong' })
   def index
     respond_to do |format|
       format.json { render json: JSON.parse('{"message":"Pong"}').to_json }
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show], constraints: { format: /(text|json)/ }
+      resources :users, only: [:index, :show], constraints: { format: /(json)/ }
     end
 
     namespace :v0 do
-      resources :ping, only: [:index], constraints: { format: /(text|json)/ }
+      resources :ping, only: [:index], constraints: { format: /(json)/ }
     end
 
   end
