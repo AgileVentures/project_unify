0827242c051cf8c1892e47f69571f19a999c19ba
tochman <thomas@nocebo.se>
2016-02-22 11:41:16 +0100

    minor changes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,19 @@
 class Api::V1::UsersController < ApiController
 
-  api :GET, '/v1/users/', 'Responds with list of users'
+
+  api :GET, '/v1/users/','List of resources: User'
+  formats ['json']
+  description  'Returns a list of registered users with URL\'s to #show for each instance'
+  example " 'user': {...} "
   def index
     @users = User.all
   end
-  
-  api :GET, '/v1/users/:id', 'Responds with a user resource'
-  param :id, Fixnum, desc: 'user_id', required: true
+
+  api :GET, '/v1/users/:id', 'Show a :resource'
+  formats ['json']
+  description  'Returns an instance of user'
+  example " 'user': {...} "
+
   def show
     @user = User.find(params[:user][:id])
   end
