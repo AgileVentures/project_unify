8ac7732f0bb174a96f8eed2609dd27309d1d96f4
Zmago Devetak <zmago_devetak@yahoo.com>
2016-02-23 09:15:48 +0000

    add user spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    user_name 'Random Guy'
+    user_name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password "password"
+    password_confirmation "password"
   end
 end
