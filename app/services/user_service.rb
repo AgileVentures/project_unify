d61f5c8c32b96fb950cc745b4727b7c4bb76e87d
Sampriti Panda <sampdingo@hotmail.com>
2016-02-25 01:57:47 +0530

    Add email, password to Godmin interface

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -2,13 +2,22 @@ class UserService
   include Godmin::Resources::ResourceService
 
   attrs_for_index :user_name, :created_at
-  attrs_for_show :user_name, :created_at
-  attrs_for_form :user_name
+  attrs_for_show :user_name, :email, :created_at
+  attrs_for_form :user_name, :email, :password, :password_confirmation
 
   filter :user_name
   batch_action :destroy, confirm: true
 
   def filter_user_name(resources, value)
     resources.where('user_name LIKE ?', "%#{value}%")
+  end
+
+  def update_resource(resource, params)
+    if params[:password].blank? then
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    resource.update(params)
   end
 end
--- features/step_definitions/godmin_steps.rb
+++ features/step_definitions/godmin_steps.rb
@@ -1,6 +1,6 @@
 Given(/^the following users exist$/) do |table|
   table.hashes.each do |hash|
-    FactoryGirl.create(:user, user_name: hash[:user_name])
+    user = FactoryGirl.create(:user, hash)
   end
 end
 
