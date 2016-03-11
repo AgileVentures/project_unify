0b93b90bb31841108d645c211338d94032dec51e
tochman <thomas@nocebo.se>
2016-03-11 17:50:41 +0100

    fix for token

--- app/services/api/v1/activity_feed.rb
+++ app/services/api/v1/activity_feed.rb
@@ -1,8 +1,8 @@
 require 'fb_graph2'
 module Api::V1::ActivityFeed
-  PROD_TOKEN = "#{ENV['FACEBOOK_APP_ID']}|#{ENV['FACEBOOK_APP_SECRET']}"
-  TOKEN = PROD_TOKEN || '1621623384764301|52edb8b7ca0af425a4cb406781004803'
-  FB_PAGE = 1660157410904515
+  #PROD_TOKEN = "#{ENV['FACEBOOK_APP_ID']}|#{ENV['FACEBOOK_APP_SECRET']}"
+  TOKEN = ENV['TOKEN']
+  FB_PAGE = '1660157410904515'
 
   def self.get_fb_feed
     page = FbGraph2::Page.new(FB_PAGE).authenticate(TOKEN).fetch
