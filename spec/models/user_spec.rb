7c3e7aa193c23b0bea1e6609da034445b2900dbf
Zmago Devetak <zmago_devetak@yahoo.com>
2016-03-17 09:39:01 +0000

    add specs to user ip_address

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -139,13 +139,19 @@ RSpec.describe User, type: :model do
   end
 
   describe 'Geocoder' do
-    let!(:user) { FactoryGirl.create(:user, user_name: 'Zmago', latitude: 45.960491, longitude: 13.6599124 ) }
-  
+    let(:user_1) { FactoryGirl.create(:user, user_name: 'Zmago', latitude: 45.960491, longitude: 13.6599124 ) }
+    let(:user_2) { FactoryGirl.create(:user, user_name: 'Thomas', ip_address: "195.41.5.202", latitude: nil, longitude:nil ) }
+    
     it 'should set the address to user' do
-     
-      expect(user.city).to eq 'Kromberk'
-      expect(user.state).to eq 'Nova Gorica'
-      expect(user.country).to eq 'Slovenia'
+      expect(user_1.city).to eq 'Kromberk'
+      expect(user_1.state).to eq 'Nova Gorica'
+      expect(user_1.country).to eq 'Slovenia'
+    end
+    
+    it 'should set the address to user by ip address when are no lat, long' do
+      expect(user_2.city).to eq 'Yıldırım'
+      expect(user_2.state).to eq 'Bursa'
+      expect(user_2.country).to eq 'Turkey'      
     end
   end
   
