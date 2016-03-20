6491c42eb989bf2f1cb98493e584db718444dc6b
tochman <thomas@nocebo.se>
2016-03-20 21:23:44 +0100

    adds mailbox#inbox specs

--- spec/requests/api/v1/mailbox_spec.rb
+++ spec/requests/api/v1/mailbox_spec.rb
@@ -7,6 +7,8 @@ describe Api::V1::MailboxController do
 
   let(:sender_headers) { {HTTP_X_USER_EMAIL: sender.email, HTTP_X_USER_TOKEN: sender.authentication_token, HTTP_ACCEPT: 'application/json'} }
   let(:receiver_1_headers) { {HTTP_X_USER_EMAIL: receiver_1.email, HTTP_X_USER_TOKEN: receiver_1.authentication_token, HTTP_ACCEPT: 'application/json'} }
+  let(:receiver_2_headers) { {HTTP_X_USER_EMAIL: receiver_2.email, HTTP_X_USER_TOKEN: receiver_2.authentication_token, HTTP_ACCEPT: 'application/json'} }
+
   let(:no_headers) { {HTTP_ACCEPT: 'application/json'} }
 
   describe 'should require authentication' do
@@ -31,17 +33,28 @@ describe Api::V1::MailboxController do
     end
   end
 
-  describe 'creating and sending a message' do
+  describe 'Inbox' do
     before do
       sender.send_message(receiver_1, 'first message', 'subject 1')
-      sender.send_message(receiver_2, 'second message', 'subject 2')
+      receiver_1.send_message(receiver_2, 'second message', 'subject 2')
+      receiver_1_conversation = receiver_1.mailbox.inbox.first
+      receiver_1.reply_to_conversation(receiver_1_conversation, 'Reply body 1')
     end
 
-    it 'displays inbox for authenticated user' do
+    it 'displays conversations while displaying receivers inbox' do
       get '/api/v1/mailbox/inbox', {}, receiver_1_headers
       first_message = response_json['inbox'].first
       expect(first_message['subject']).to eq 'subject 1'
       expect(first_message['from']['user_name']).to eq 'Thomas'
+      expect(first_message['messages'].count).to eq 2
+    end
+
+    it 'does not display conversation while displaying other users inbox' do
+      get '/api/v1/mailbox/inbox', {}, receiver_2_headers
+      first_message = response_json['inbox'].first
+      expect(first_message['subject']).not_to eq 'subject 1'
+      expect(first_message['from']['user_name']).not_to eq 'Thomas'
+      expect(first_message['messages'].count).not_to eq 2
     end
   end
 
