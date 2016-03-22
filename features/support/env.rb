9b02a87bc67e333e9a5f35d94cf0ebfb9ddfe605
Thomas Ochman <thomas@nocebo.se>
2016-03-22 12:24:21 +0100

    Revert "Coverall cleanup"

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,10 @@
 require 'simplecov'
 require 'coveralls'
 SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start 'rails' do
+  add_filter 'app/secrets'
+end
 Coveralls.wear_merged!
-SimpleCov.command_name "Cucumber"
 require 'rack/test'
 require 'cucumber/rails'
 require 'capybara/poltergeist'
