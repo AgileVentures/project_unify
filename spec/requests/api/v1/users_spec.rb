7da289d3a9b5a336073bd75d8f76eaa5faa66263
mtc2013 <mtc2013opensource@gmail.com>
2016-08-01 14:16:08 -0400

    Add json of pending invited friends to api endpoint (#73)

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -61,7 +61,8 @@ describe Api::V1::UsersController do
                                   skills: resource.skill_list.sort,
                                   created_at: resource.created_at,
                                   friends: resource.friends,
-                                  pending_friendships: resource.pending_invited_by}}
+                                  pending_friendships: resource.pending_invited_by,
+                                  pending_invited_friendships: []}}
       expect(response_json).to eq JSON.parse(expected_response.to_json)
     end
 
